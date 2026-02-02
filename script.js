e43112d65d7e0ede0ef9d034c2e45af4a8573b73
Dafne <dafnej@nycstudents.net>
2026-02-02 13:44:46 -0500

    yoo

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ var key = "";
 
 // SAVE
 document.querySelector(".save").onclick = function() {
-
+  key = document.querySelector("#saveKey").value;
+    var value = document.querySelector("#saveValue").value;
+    localStorage.setItem(key, value);
 };
 
 
 // LOAD
 document.querySelector(".load").onclick = function() {
-
+  key = document.querySelector("#loadKey").value;
+    var value = localStorage.getItem(key);
+    document.querySelector("#loadValue").value = value;
 };
 
 // LOAD
 document.querySelector(".update").onclick = function() {
-
+  key = document.querySelector("#updateKey").value;
+    var value = document.querySelector("#updateValue").value;
+    localStorage.setItem(key, value);
 };
 
 // DELETE
 document.querySelector(".delete").onclick = function() {
-
+  key = document.querySelector("#deleteKey").value;
+    localStorage.removeItem(key);
+    document.querySelector("#deleteKey").value = ""
 };
