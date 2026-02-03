fe1c6ca66bc7b62d342c40feada6afb26e9c5605
Dafne <dafnej@nycstudents.net>
2026-02-03 13:55:44 -0500

    SWEET

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-var key = "";
+var key = "myDataKey";
 
 
 // SAVE
 document.querySelector(".save").onclick = function() {
-  key = document.querySelector("#saveKey").value;
-    var value = document.querySelector("#saveValue").value;
-    localStorage.setItem(key, value);
+var inputValue = document.getElementById("saveValue").value;
+localStorage.setItem(key, inputValue);
+document.getElementById("saveValue").value = "";
 };
-
 
 // LOAD
 document.querySelector(".load").onclick = function() {
-  key = document.querySelector("#loadKey").value;
-    var value = localStorage.getItem(key);
-    document.querySelector("#loadValue").value = value;
+var storedValue = localStorage.getItem(key);
+if (storedValue === null) {
+document.getElementById("loadValue").value = key + " has been deleted.";
+} else {
+document.getElementById("loadValue").value = storedValue;
+}
 };
 
-// UPDATE 
+// UPDATE
 document.querySelector(".update").onclick = function() {
-  key = document.querySelector("#updateKey").value;
-    var value = document.querySelector("#updateValue").value;
-    localStorage.setItem(key, value);
+var newValue = document.getElementById("updateValue").value;
+localStorage.setItem(key, newValue);
+document.getElementById("updateValue").value = "";
 };
 
 // DELETE
 document.querySelector(".delete").onclick = function() {
-  key = document.querySelector("#deleteKey").value;
-    localStorage.removeItem(key);
-    document.querySelector("#deleteKey").value = ""
+var deleteKeyInput = document.getElementById("deleteKey").value;
+var keyToDelete = deleteKeyInput;
+localStorage.removeItem(key);
+document.getElementById("deleteKey").value = "";
 };
+
+function deleteData() {
+var deleteKeyInput = document.getElementById("deleteKey").value;
+var keyToDelete = deleteKeyInput;
+localStorage.removeItem(keyToDelete);
+document.getElementById("deleteKey").value = "";
+}
+
